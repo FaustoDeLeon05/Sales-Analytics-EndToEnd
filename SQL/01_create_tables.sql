42ed65d947303ec5cf8c7da29f539598516b3ea1
Fausto Xavier de Leon Pichardo <faustoxavier20@gmail.com>
2026-09-11 00:14:05 -0400

    Update 01_create_tables.sql
    Eliminando backup.

--- SQL/01_create_tables.sql
+++ SQL/01_create_tables.sql
@@ -73,21 +73,3 @@ BEGIN
         ON dbo.Dim_Gastos ([Fecha]);
 END;
 GO
-
-
-
-/*
-USE master;
-GO
-
-
-    Optional local backup.
-    Update the destination path for the SQL Server environment where the
-    backup is executed. This script is intentionally separate from analysis.
-
-
-BACKUP DATABASE GlobalSalesDB
-TO DISK = 'C:\SQL Backups\GlobalSalesDB.bak'
-WITH FORMAT;
-GO
-*/
